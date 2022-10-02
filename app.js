a6b1c9a05b899dffe32ccc0cf115f63a5513979e
Soumya624 <soumyatarafder624@iitkgp.ac.in>
2022-10-02 22:21:44 +0530

    Added

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const signup = require('./routes/signup');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/signup', signup);
+
+app.get('/', (req,res)=>{
+    res.send('We are on Backend Server');
+});
+
+mongoose.connect('mongodb+srv://user1:hH0daYK4xW4qsDl9@cluster0.gco9idg.mongodb.net/?retryWrites=true&w=majority', ()=>{
+    console.log('Connected to MongoDB');
+})
+
+app.listen(5000);
