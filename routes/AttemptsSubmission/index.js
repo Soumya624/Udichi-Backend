8681ff14e0e1e1d0733eae96f1fc41bdd5ddad4f
Subhojit-Dey1234 <subhojit2000dey@gmail.com>
2022-10-29 16:20:55 +0530

    Download

--- routes/AttemptsSubmission/index.js
+++ routes/AttemptsSubmission/index.js
@@ -5,11 +5,13 @@ const router = express.Router();
 
 const createAttempts = require("./createAttempts");
 const createAttemptsGroup = require("./createAttemptsGroup");
+const downloadAttemptsById = require("./DownloadAttempts");
 const editAttempts = require("./editAttempts");
 const editAttemptsGroup = require("./editAttemptsGroup");
 const getAttemptsById = require("./getAttemptsById");
 const getAttemptsByTestId = require("./getAttemptsByTestId");
 
+router.get("/download/:id",downloadAttemptsById)
 router.get("/attempts_group/:test", getAttemptsByTestId);
 router.get("/attempt/:id/:candidate", getAttemptsById);
 router.get("/group/:candidate/:test", checkAttemptGroup);
