9de6c240f066b646edc1a1b37ce2114bcfe2f4db
Soumya624 <soumyatarafder624@iitkgp.ac.in>
2022-10-29 15:44:17 +0530

    Added

--- routes/AttemptsSubmission/getAttemptsByTestId.js
+++ routes/AttemptsSubmission/getAttemptsByTestId.js
@@ -1,13 +1,18 @@
 const AttemptSchemaGroup = require("../../models/AttemptsGroupSchema");
 
 const getAttemptsByTestId = async (req, res) => {
+	console.log(req.params)
 	try {
 		let attempt_schema = await AttemptSchemaGroup.find({
-			test: req.body.test,
-		});
-		if (attempt_schema.length() === 0) res.status(404).send("Not Found");
+			test: req.params.test,
+		})
+		.populate({
+			path : "candidate",
+			model : "candidate_schema"
+		})
+		if (attempt_schema.length === 0) res.status(404).send("Not Found");
 
-		res.status(200).send(attempt_schema[0]);
+		res.status(200).send(attempt_schema);
 	} catch (error) {
 		res.status(500).json(error.message);
 	}
