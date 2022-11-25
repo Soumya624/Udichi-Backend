9578031e1c5c5f00aabea77a231fe0ca075d9ea1
Subhojit-Dey1234 <subhojit2000dey@gmail.com>
2022-11-25 11:37:26 +0530

    Render Hosting

--- routes/AttemptsSubmission/uploadMulter.js
+++ routes/AttemptsSubmission/uploadMulter.js
@@ -2,7 +2,7 @@ const path = require("path");
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-	destination: "/tmp/AttemptsZipFile/",
+	destination: "./media/AttemptsZipFile/",
 	filename: function (req, file, cb) {
 		cb(null, "ATTEMPTS" + Date.now() + path.extname(file.originalname));
 	},
