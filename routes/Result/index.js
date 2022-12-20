5bc79c989b892535818fa63dd3edbde491bea094
Subhojit-Dey1234 <subhojit2000dey@gmail.com>
2022-12-20 18:43:23 +0530

    Error

--- routes/Result/index.js
+++ routes/Result/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const getResultByAttemptsIdAndTestId = require('./getResultByAttemptsIdAndTestId')
 const router = express.Router()
-
+const authMiddleware = require('../../Middlewares/authMiddleware')
 
 router.patch('/:test_id/:attempt_id',[authMiddleware],getResultByAttemptsIdAndTestId)
 
